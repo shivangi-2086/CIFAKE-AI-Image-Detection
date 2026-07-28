6adb49b278bcfcb4c9dbf1f6208827d72c837ed9
Shivangi <shivangi2086@gmail.com>
2026-07-28 20:42:54 +0530

    Fix model loading path in app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image
 
-model = tf.keras.models.load_model("model/cifake_model.keras")
+model = tf.keras.models.load_model("cifake_model.keras")
 
 st.title("🖼️ CIFAKE - AI Image Detector")
 
